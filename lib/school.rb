e29493c609eca7d26a18cfe2d1950f6ad12b037d
allison_johnson <allison.cuttler@gmail.com>
2019-12-11 15:24:12 -0800

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end #init
+
+  def add_student(stud_name, stud_grade)
+    @roster[stud_grade] = [] if !@roster.keys.include?(stud_grade)
+    @roster[stud_grade] << stud_name
+  end
+
+  def grade(a_grade)
+    @roster[a_grade]
+  end 
+
+  def sort
+    @roster.each do |grade, students|
+        students.sort!
+    end #each
+  end 
+end #class
